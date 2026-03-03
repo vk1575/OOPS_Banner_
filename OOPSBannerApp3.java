7dbd92b67581122076bd1c14e62f2e949c65373e
vk1575 <vk1575@srmist.edu.in>
2026-03-03 14:20:57 +0530

    Add files via upload

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,13 @@
+class OOPSBannerApp3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("", "      ***      ***      *****       ***** "));
+        System.out.println(String.join("", "     ** **    ** **    **   **    **      "));
+        System.out.println(String.join("", "    **   **  **   **   **   **   **       "));
+        System.out.println(String.join("", "    **   **  **   **   *****      ***     "));
+        System.out.println(String.join("", "    **   **  **   **   **            **   "));
+        System.out.println(String.join("", "     ** **    ** **    **             **  "));
+        System.out.println(String.join("", "      ***      ***     **        *****    "));
+
+    }
+}
